d4073c3390e8bf93d84cbc37168a48721c584a69
David Dufour-Boivin <david.dufour-boivin@zalando.de>
2016-12-22 19:05:31 +0100

    Additional methods + full test for every one of them

--- src/main/scala/com/github/adeynack/scala/concurrent/FutureOfEither.scala
+++ src/main/scala/com/github/adeynack/scala/concurrent/FutureOfEither.scala
@@ -7,30 +7,44 @@ object FutureOfEither {
   implicit class FutureOfEitherExtensions[A, B](val underlying: Future[Either[A, B]]) {
 
     /**
-      * Maps the [[Right]] value through a function that returns a [[Future]] of the new [[Right]] value.
+      * Maps the [[Right]] value through a function that returns a new [[Right]] value.
       *
       * - Can be changed to a `Left`:   No
       * - Value of `Right` can change:  Yes
       * - Type of `Right` can change:   Yes
       * - Value returned as Future:     No
+      */
+    def rightMap[S](f: B => S)(implicit executor: ExecutionContext): Future[Either[A, S]] = underlying.flatMap {
+      case Left(a) => Future.successful(Left[A, S](a))
+      case Right(b) => Future(f(b)).map(Right[A, S])
+    }
+
+    /**
+      * Maps the [[Right]] value through a function that returns a [[Future]] of the new [[Right]] value.
+      *
+      * - Can be changed to a `Left`:   No
+      * - Value of `Right` can change:  Yes
+      * - Type of `Right` can change:   Yes
+      * - Value returned as Future:     Yes
       *
       */
-    def rightMap[S](f: B => Future[S])(implicit executor: ExecutionContext): Future[Either[A, S]] = underlying.flatMap {
+    def rightFlatMap[S](f: B => Future[S])(implicit executor: ExecutionContext): Future[Either[A, S]] = underlying.flatMap {
       case Left(a) => Future.successful(Left[A, S](a))
       case Right(b) => f(b).map(Right[A, S])
     }
 
-    // todo: rightFlatMap
-    //    * - Can be changed to a `Left`:   No
-    //    * - Value of `Right` can change:  Yes
-    //    * - Type of `Right` can change:   Yes
-    //    * - Value returned as Future:     Yes
-
-    // todo: rightMapWith
-    //    * - Can be changed to a `Left`:   Yes
-    //    * - Value of `Right` can change:  Yes
-    //    * - Type of `Right` can change:   Yes
-    //    * - Value returned as Future:     No
+    /**
+      * Maps the [[Right]] value through a function that returns a new [[Either]] value.
+      *
+      * - Can be changed to a `Left`:   Yes
+      * - Value of `Right` can change:  Yes
+      * - Type of `Right` can change:   Yes
+      * - Value returned as Future:     No
+      */
+    def rightMapWith[S](f: B => Either[A, S])(implicit executor: ExecutionContext): Future[Either[A, S]] = underlying.flatMap {
+      case Left(a) => Future.successful(Left[A, S](a))
+      case Right(b) => Future(f(b))
+    }
 
     /**
       * Maps the [[Right]] value through a function that returns a [[Future]] of the new [[Either]] value (can
@@ -45,6 +59,20 @@ object FutureOfEither {
     def rightFlatMapWith[S](f: B => Future[Either[A, S]])(implicit executor: ExecutionContext): Future[Either[A, S]] = underlying.flatMap {
       case Left(a) => Future.successful(Left[A, S](a))
       case Right(b) => f(b)
+    }
+
+    /**
+      * Maps the [[Right]] value through a function that returns a [[Future]] of the new [[Right]] value.
+      *
+      * - Can be changed to a `Left`:   Yes
+      * - Value of `Right` can change:  Yes
+      * - Type of `Right` can change:   No
+      * - Value returned as Future:     No
+      *
+      */
+    def rightMapWithPF(f: PartialFunction[B, Either[A, B]])(implicit executor: ExecutionContext): Future[Either[A, B]] = underlying.flatMap {
+      case _: Left[A, B] => underlying
+      case r: Right[A,B] => Future(f.applyOrElse(r.value, (_: B) => r))
     }
 
     /**
--- src/test/scala/com/github/adeynack/scala/concurrent/FutureOfEitherDemoSpec.scala
+++ src/test/scala/com/github/adeynack/scala/concurrent/FutureOfEitherDemoSpec.scala
@@ -9,6 +9,190 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class FutureOfEitherDemoSpec extends FreeSpec with Matchers {
 
+  private def success[A](value: A): Future[Either[String, A]] = Future.successful(Right[String, A](value))
+
+  "Methods" - {
+
+    "rightMap" - {
+
+      "maps a right value" in {
+        val f = success("42").rightMap(_.toInt)
+        Await.result(f, 1.minute).right.get shouldEqual 42
+      }
+
+      "maps a right value with failure" in {
+        class TestException extends Exception
+        val f = success("asd").rightMap(_ => throw new TestException)
+        intercept[TestException] {
+          Await.result(f, 1.minute)
+        }
+      }
+
+    }
+
+    "rightFlatMap" - {
+
+      "flat maps a right value from another future" in {
+        val f = success("42").rightFlatMap(s => Future(s.toInt))
+        Await.result(f, 1.minute).right.get shouldEqual 42
+      }
+
+      "flat maps a right value from another failing future" in {
+        val f = Future.successful(Right[Int, String]("asd")).rightFlatMap(s => Future(s.toInt))
+        intercept[NumberFormatException] {
+          Await.result(f, 1.minute)
+        }
+      }
+
+      "flat maps a right value from another future ... but creating that future fails" in {
+        class TestException extends Exception
+        val f = Future.successful(Right[Int, String]("asd")).rightFlatMap(_ => throw new TestException)
+        intercept[TestException] {
+          Await.result(f, 1.minute)
+        }
+      }
+
+      "flat maps a right value from a left value" in {
+        val f = Future.successful(Left[Int, String](-1)).rightFlatMap(s => Future(s.toInt))
+        Await.result(f, 1.minute).left.get shouldEqual -1
+      }
+
+    }
+
+    "rightMapWith" - {
+
+      "maps a right value to another Either" in {
+        val f = success("42").rightMapWith(s => Right(Seq(s, "Life", "Universe", "Everything")))
+        Await.result(f, 1.minute).right.get shouldEqual Seq("42", "Life", "Universe", "Everything")
+      }
+
+      "maps a right value to another Either with failure" in {
+        class TestException extends Exception
+        val f = success("42").rightMapWith(_ => throw new TestException)
+        intercept[TestException] {
+          Await.result(f, 1.minute)
+        }
+      }
+
+      "maps a right value to a left" in {
+        val f = success("42").rightMapWith(s => Left("I do not like this value"))
+        Await.result(f, 1.minute).left.get shouldEqual "I do not like this value"
+      }
+
+    }
+
+    "rightFlatMapWith" - {
+
+      "flat maps a right value to another future of either" in {
+        val f = success("42").rightFlatMapWith(s => Future(Right(Seq(s, "Life", "Universe", "Everything"))))
+        Await.result(f, 1.minute).right.get shouldEqual Seq("42", "Life", "Universe", "Everything")
+      }
+
+      "maps a right value to another failure future of either" in {
+        class TestException extends Exception
+        val f = success("42").rightFlatMapWith(s => Future.failed(new TestException))
+        intercept[TestException] {
+          Await.result(f, 1.minute)
+        }
+      }
+
+      "maps a right value to another future ... but creating that future fails" in {
+        class TestException extends Exception
+        val f = success("42").rightFlatMapWith(_ => throw new TestException)
+        intercept[TestException] {
+          Await.result(f, 1.minute)
+        }
+      }
+
+      "maps a right value to another future of left" in {
+        val f = success("42").rightFlatMapWith(s => Future(Left("I do not like this value")))
+        Await.result(f, 1.minute).left.get shouldEqual "I do not like this value"
+      }
+
+    }
+
+    "rightMapWithPF" - {
+
+      "maps a right value to another value using a partial function that matches and returns another right" in {
+        val f = success("42").rightMapWithPF {
+          case "42" => Right("Life, Universe, Everything!")
+        }
+        Await.result(f, 1.minute).right.get shouldEqual "Life, Universe, Everything!"
+      }
+
+      "maps a right value to the same value using a partial function that does not match" in {
+        val f = success("41").rightMapWithPF {
+          case "42" => Right("Life, Universe, Everything!")
+        }
+        Await.result(f, 1.minute).right.get shouldEqual "41"
+      }
+
+      "maps a right value using a partial function that fails" in {
+        class TestException extends Exception
+        val f = success("42").rightMapWithPF {
+          case _ => throw new TestException
+        }
+        intercept[TestException] {
+          Await.result(f, 1.minute)
+        }
+      }
+
+      "maps a right value to another value using a partial function that matches and returns a left" in {
+        val f = success("41").rightMapWithPF {
+          case "41" => Left("Not fun...")
+        }
+        Await.result(f, 1.minute).left.get shouldEqual "Not fun..."
+      }
+
+    }
+
+    "rightFlatMapWithPF" - {
+
+      "maps a right value to a future of another value using a partial function that matches and returns another right" in {
+        val f = success("42").rightFlatMapWithPF {
+          case "42" => Future(Right("Life, Universe, Everything!"))
+        }
+        Await.result(f, 1.minute).right.get shouldEqual "Life, Universe, Everything!"
+      }
+
+      "maps a right value to a future the same value using a partial function that does not match" in {
+        val f = success("41").rightFlatMapWithPF {
+          case "42" => Future(Right("Life, Universe, Everything!"))
+        }
+        Await.result(f, 1.minute).right.get shouldEqual "41"
+      }
+
+      "maps a right value to a future using a partial function that fails" in {
+        class TestException extends Exception
+        val f = success("42").rightFlatMapWithPF {
+          case _ => Future.failed(new TestException)
+        }
+        intercept[TestException] {
+          Await.result(f, 1.minute)
+        }
+      }
+
+      "maps a right value to a future using a partial function ... but that future creation fails" in {
+        class TestException extends Exception
+        val f = success("42").rightFlatMapWithPF {
+          case _ => throw new TestException
+        }
+        intercept[TestException] {
+          Await.result(f, 1.minute)
+        }
+      }
+
+      "maps a right value to a future another value using a partial function that matches and returns a left" in {
+        val f = success("41").rightFlatMapWithPF {
+          case "41" => Future(Left("Not fun..."))
+        }
+        Await.result(f, 1.minute).left.get shouldEqual "Not fun..."
+      }
+
+    }
+
+  }
+
   "Use cases" - {
 
     "Authentication" - {
@@ -96,7 +280,7 @@ class FutureOfEitherDemoSpec extends FreeSpec with Matchers {
           // Value of `Right` can change ... Yes
           // Type of `Right` can change .... Yes
           //
-          .rightMap {
+          .rightFlatMap {
             case RawUser(name, id) => Future.successful[User](User(s"ID:$id", name))
           }
           //
